3fbfaa076ca671156863c890c9fed7fd56389d8c
MiBakic <milica.cvrle.93@gmail.com>
2022-09-25 17:38:05 +0200

    Changing Main method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -17,26 +17,30 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Bakic\\Desktop\\selenium\\chromedriver.exe");
         WebDriver driver= new ChromeDriver();
         driver.get("http://automationpractice.com");
-        driver.manage().window().maximize();
-//
-        WebElement product1= driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[4]/div/div[2]/div[2]/a[1]/span" ));
-        product1.click();
 
-        WebElement continueShoppingBtn1= driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span/span/i"));
-        continueShoppingBtn1.click();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        WebElement quickView1 = driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[1]/div/a[1]/img"));
+        quickView1.click();
+        WebElement addToCart1 = driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button/span"));
+        addToCart1.click();
+        WebElement continue1 = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span"));
+        continue1.click();
 
-        Wait<WebDriver> waiter1= new FluentWait<>(driver).withTimeout(Duration.ofSeconds(15)).pollingEvery(Duration.ofSeconds(2));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        WebElement quickView2 = driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[2]/div/div[1]/div/a[2]/span"));
+        quickView1.click();
+        WebElement addToCart2 = driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button/span"));
+        addToCart1.click();
+        WebElement continue2 = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span"));
+        continue1.click();
 
-        WebElement product2= driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[2]/div/div[2]/div[2]/a[1]/span"));
-        product2.click();
-
-        continueShoppingBtn1.click();
-        Wait<WebDriver> waiter2= new FluentWait<>(driver).withTimeout(Duration.ofSeconds(15)).pollingEvery(Duration.ofSeconds(2));
-
-        WebElement product3= driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[1]/div/div[2]/div[2]/a[1]/span"));
-        product3.click();
-
-        WebDriverWait driverWait= new WebDriverWait(driver,Duration.ofSeconds(6));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        WebElement quickView3 = driver.findElement(By.xpath("//*[@id=\"homefeatured\"]/li[3]/div/div[1]/div/a[2]"));
+        quickView1.click();
+        WebElement addToCart3 = driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button/span"));
+        addToCart1.click();
+        WebElement continue3 = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/span"));
+        continue1.click();
 
         WebElement cartBtn= driver.findElement(By.xpath("//*[@id=\"header\"]/div[3]/div/div/div[3]/div/a"));
         cartBtn.click();
